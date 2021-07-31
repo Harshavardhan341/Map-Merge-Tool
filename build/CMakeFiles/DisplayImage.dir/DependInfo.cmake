236ba65251689f40579f5e2d74953efd91d6762c
Harshavardhan341 <harshavardhan.kulkarni@learner.manipal.edu>
2021-07-31 21:34:44 +0530

    changed CMakeFiles

--- build/CMakeFiles/DisplayImage.dir/DependInfo.cmake
+++ build/CMakeFiles/DisplayImage.dir/DependInfo.cmake
@@ -4,14 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ramune/catkin_ws/src/Map-Merge-Tool/main.cpp" "/home/ramune/catkin_ws/src/Map-Merge-Tool/build/CMakeFiles/DisplayImage.dir/main.cpp.o"
-  "/home/ramune/catkin_ws/src/Map-Merge-Tool/mapmerge.cpp" "/home/ramune/catkin_ws/src/Map-Merge-Tool/build/CMakeFiles/DisplayImage.dir/mapmerge.cpp.o"
+  "/home/hk/Map-Merge-Tool/main.cpp" "/home/hk/Map-Merge-Tool/build/CMakeFiles/DisplayImage.dir/main.cpp.o"
+  "/home/hk/Map-Merge-Tool/mapmerge.cpp" "/home/hk/Map-Merge-Tool/build/CMakeFiles/DisplayImage.dir/mapmerge.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/opencv"
+  "/usr/local/include/opencv4"
   )
 
 # Targets to which this target links.
